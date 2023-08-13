6c8e1e0342db564c8c068232e6aded03d7636ea7
Atishaya Maharjan <atishaya7777@gmail.com>
2023-08-13 15:35:20 -0500

    feat: Added storybook and configurations for jest, babel, webpack, and storybook addons

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import packageJson from "./package.json" assert { type: "json" };
 import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default [
   {
